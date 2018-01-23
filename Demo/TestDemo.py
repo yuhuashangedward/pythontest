b18ea0de3ae1dd138fbabb0bd657d47f99d94386
yuhuashangedward <yuhuashang@aliyun.com>
2018-01-23 20:13:35 +0800

    hello

--- Demo/TestDemo.py
+++ Demo/TestDemo.py
@@ -1,1 +1,19 @@
-print('Hello World!')
+#print('Hello World!')
+
+#时间模块--datetime
+#import time
+#from datetime import date
+#today = date.today()
+#print(today)
+
+#事件模块
+import time
+from datetime import date
+today=date.today()
+today.isoformat()
+print(today.isoformat())
+today.strftime("%d/%m/%y")
+
+today.strftime("%d-%m-%y")
+
+today.strftime("%A %d.%B %Y")
