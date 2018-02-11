37523b8522b73fda579014a44b8db682456f9dc3
yuhuashangedward <yuhuashang@aliyun.com>
2018-02-11 08:57:24 +0800

    测试测试

---
+++ web/crawler/DemoWeb08.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.get("https://www.baidu.com")
+print(type(response))
+print(response.status_code)
+print(type(response.text))
+
+response.encoding = "utf-8"
+print(response.text)
+
+print(response.cookies)
+
+print(response.content)
+print(response.content.decode("utf-8"))
